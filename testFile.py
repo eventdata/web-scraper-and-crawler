5d540af10d6cd50c05176c9cb821234b8d6130d7
sxs149331 <sxs149331@utdallas.edu>
2017-10-27 18:06:46 -0500

    Updated list of sites to crawl.

--- testFile.py
+++ testFile.py
@@ -11,7 +11,7 @@ article_count = 0
 extractor = NewspaperTextExtractor(language="es")
 
 
-response = requests.get("http://caracol.com.co/emisora/2017/10/27/bogota/1509106310_741399.html")
+response = requests.get("http://caracol.com.co/emisora/2017/09/08/sincelejo/1504868022_942997.html")
 
 print response.content
 
@@ -22,6 +22,7 @@ article = extractor.extractAll(response.content)
 # article.download()
 #
 # article.parse()
+print type(response)
 
 if article.publish_date is  None:
     print "Date missing"
